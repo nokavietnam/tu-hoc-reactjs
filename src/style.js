f7e195356687b1e8ea0b374058732e6c04dd7e2a
noka <noka.hieunt@gmail.com>
2022-01-27 20:21:46 +0700

    update demo

---
+++ src/style.js
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(0, 8, 0),
+  },
+  icon: {
+    margin: '20px',
+  },
+  button: {
+    margin: '40px',
+  },
+  cardGrid: {
+    padding: '20px 0',
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%',
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: '50px 0',
+  },
+}));
+
+export default useStyles;
